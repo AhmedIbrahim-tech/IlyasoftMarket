7a02d990142690a4743cf360837d17e11cc8771c
Ahmed Ibrahim <ebrahema89859@gmail.com>
2023-12-18 20:23:58 +0200

    [Add] UI Design [UOMO Template]

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -15,5 +15,10 @@ namespace Web.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
--- Web/Program.cs
+++ Web/Program.cs
@@ -3,7 +3,7 @@ using Infrastructure.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 
 #region Connection Database
